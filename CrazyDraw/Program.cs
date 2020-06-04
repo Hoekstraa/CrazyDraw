c0d14a53cd8a315fb5aa21a400032b93fd02620e
JanSHoekstra <altlockg@gmail.com>
2020-06-04 09:43:59 +0200

    Add project files.

---
+++ CrazyDraw/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.Color;
+
+namespace CrazyDraw
+{
+    class Program
+    {
+        public static int Main()
+        {
+            // Initialization
+            //--------------------------------------------------------------------------------------
+            const int screenWidth = 800;
+            const int screenHeight = 600;
+
+            InitWindow(screenWidth, screenHeight, "CrazyDraw");
+
+            SetTargetFPS(60);
+            //--------------------------------------------------------------------------------------
+
+            // Main game loop
+            while (!WindowShouldClose())    // Detect window close button or ESC key
+            {
+                // Update
+
+                // Draw
+                //----------------------------------------------------------------------------------
+                BeginDrawing();
+
+                ClearBackground(RAYWHITE);
+
+                DrawText("Congrats! You created your first window!", 190, 200, 20, MAROON);
+
+                EndDrawing();
+                //----------------------------------------------------------------------------------
+            }
+
+            // De-Initialization
+            //--------------------------------------------------------------------------------------
+            CloseWindow();        // Close window and OpenGL context
+            //--------------------------------------------------------------------------------------
+
+            return 0;
+        }
+    }
+}
